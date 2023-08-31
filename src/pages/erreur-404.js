1977c74a911d78bf0fbbfd657a0523cd32d5e2b4
matthieu-h <matthieu.hatinguais@hotmail.fr>
2023-08-31 14:12:54 +0200

    intégration page 404 en desktop

--- src/pages/erreur-404.js
+++ src/pages/erreur-404.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { home } from "../routes";
 
 const Error404 = () => {
-  return <h1>404</h1>;
+  return (
+    <div className="error404">
+      <h1 className="error404__title">404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to={home} className="error404__link">
+        Retourner sur la page d'accueil
+      </NavLink>
+    </div>
+  );
 };
 
 export default Error404;
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
 export const home = "/";
-export const error404 = "/*";
+export const error404 = "*";
 export const about = "/a-propos";
 export const housing = "/logement/:logementId";
