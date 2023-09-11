67134d1e154dc0dd2b59e9c29d462b429f62fd76
matthieu-h <matthieu.hatinguais@hotmail.fr>
2023-09-11 18:56:54 +0200

    ajout responsive sur page fiche-logement

--- src/pages/a-propos.js
+++ src/pages/a-propos.js
@@ -6,30 +6,32 @@ const About = () => {
   return (
     <>
       <Banner src="./pictures/kalen-emsley-Bkci_8qcdvQ-unsplash 2 (1).png" />
-      <Collapse
-        title="Fiabilité"
-        content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+      <div className="about">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
         photos sont conformes aux logements, et toutes les informations sont
         régulièrement vérifiées par nos équipes."
-      />
-      <Collapse
-        title="Respect"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
         perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Service"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+        />
+        <Collapse
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
         perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Sécurité"
-        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
         correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
         locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
         également des ateliers sur la sécurité domestique pour nos hôtes.
         "
-      />
+        />
+      </div>
     </>
   );
 };
