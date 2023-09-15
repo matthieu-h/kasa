4592709b28e403b2b486c876a400035b11625f78
matthieu-h <matthieu.hatinguais@hotmail.fr>
2023-09-15 17:17:59 +0200

    finalisation css

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -27,25 +27,27 @@ const Slideshow = ({ pictures }) => {
   if (length >= 1) {
     return (
       <div className="housing__slideshow">
-        <img
-          src={pictures[index]}
-          alt="photos du logement"
-          className="housing__slideshow-picture"
-        />
-        <img
-          src="/pictures/arrow-left.svg"
-          alt="flèche gauche"
-          className="housing__slideshow-arrow-left"
-          onClick={previousArrow}
-        />
-        <img
-          src="/pictures/arrow-right.svg"
-          alt="flèche droite"
-          className="housing__slideshow-arrow-right"
-          onClick={nextArrow}
-        />
-        <div className="housing__slideshow-index">
-          {index + 1}/{length}
+        <div className="housing__slideshow-container">
+          <img
+            src={pictures[index]}
+            alt="photos du logement"
+            className="housing__slideshow-picture"
+          />
+          <img
+            src="/pictures/arrow-left.svg"
+            alt="flèche gauche"
+            className="housing__slideshow-arrow-left"
+            onClick={previousArrow}
+          />
+          <img
+            src="/pictures/arrow-right.svg"
+            alt="flèche droite"
+            className="housing__slideshow-arrow-right"
+            onClick={nextArrow}
+          />
+          <div className="housing__slideshow-index">
+            {index + 1}/{length}
+          </div>
         </div>
       </div>
     );
